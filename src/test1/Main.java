4dc5dd563e057bf41b18434deef751483de0d893
xuhongyuan <xuhongyuan666@163.com>
2015-05-11 23:36:30 +0800

    回文

--- src/test1/Main.java
+++ src/test1/Main.java
@@ -1,49 +1,70 @@
-	package test1;
+package test1;
 
-	import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * @author Administrator 输出所有子串
+ */
+public class Main {
+
 	/**
-	 * @author Administrator 输出所有子串
+	 * @param args
 	 */
-	public class Main {
-
-		/**
-		 * @param args
-		 */
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			Scanner scan = new Scanner(System.in);
-			String str = scan.nextLine();
-			Main program=new Main();
-			program.printAllSubString(str);
-			program.printAllUniqueString(str);
-		}
-
-		public void printAllSubString(String str){
-			for (int i = 0; i <= str.length(); i++) {
-				for (int j = i + 1; j <= str.length(); j++) {
-					System.out.println("含重复集合");
-					System.out.println(str.subSequence(i, j));
-				}
-			}	
-		}
-		public void printAllUniqueString(String str){
-			HashMap<String, String> hashmap=new HashMap<>();
-				for (int i = 0; i <= str.length(); i++) {
-					for (int j = i + 1; j <= str.length(); j++) {
-						hashmap.put(str, "");
-					}
-				}
-				for(Map.Entry<String , String> entry : hashmap.entrySet()){
-					   //System.out.println("Key="+entry.getKey()+"---->value="+entry.getValue().toString());
-					System.out.println("不重复集合");
-					   System.out.println(entry.getKey());
-					}
-		}
-		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		Main program = new Main();
+		program.printAllSubString(str);
+		System.out.println("------");
+		program.printAllUniqueString(str);
+		if (program.ifHuiWen(str))
+			System.out.println("是回文字符串");
+		else
+			System.out.println("不是回文字符串");
 	}
 
+	public void printAllSubString(String str) {// 输出所有子串
+		for (int i = 0; i <= str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				System.out.println(str.subSequence(i, j));
+			}
+		}
+	}
 
+	public void printAllUniqueString(String str) {// 输出不重复的子串
+		HashMap<String, String> hashmap = new HashMap<>();
+		for (int i = 0; i <= str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				hashmap.put(str.subSequence(i, j).toString(), "");
+			}
+		}
+		for (Map.Entry<String, String> entry : hashmap.entrySet()) {
+			// System.out.println("Key="+entry.getKey()+"---->value="+entry.getValue().toString());
+			System.out.println(entry.getKey());
+		}
+	}
+
+	public boolean ifHuiWen(String str) {// 判断是否是回文字符串
+	/*
+	 * boolean flag=true; for(int i=0;i<=(str.length()+1)/2-1;i++){
+	 * if(str.charAt(i)!=str.charAt(str.length()-1-i)) flag=false; }
+	 */
+
+		int j = str.length() - 1;
+		int i = 0;
+		while (i < j) {
+			if (str.charAt(i) == str.charAt(j)) {
+				i++;
+				j--;
+			} else
+				return false;
+		}
+
+		return true;
+	}
+
+}
