9277d2fb361ad3287e52db9747fbe3ff3f4c18d6
xuhongyuan <xuhongyuan666@163.com>
2015-06-12 14:52:09 +0800

    Speech Patterns(待修正)

--- src/test41/Main.java
+++ src/test41/Main.java
@@ -1,5 +1,7 @@
 package test41;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,6 +18,7 @@ getCommonlyWord(scan.nextLine());
 	}
 
 	public static void getCommonlyWord(String str){
+		//过滤特殊字符
 		String [] words=str.split(" ");
 		for(int i=0;i<words.length;i++){
 			String regEx = "[^A-Za-z0-9]";
@@ -23,8 +26,30 @@ getCommonlyWord(scan.nextLine());
             Matcher m = p.matcher(words[i]);
             words[i]=m.replaceAll("").trim();
 		}
+		//将各个单词进行统计放入wordMap
+		Map<String,Integer> wordMap=new HashMap<String,Integer>();
 		for(int i=0;i<words.length;i++){
-			System.out.println(words[i]);
+			if(wordMap.containsKey(words[i])){
+				int value=wordMap.get(words[i])+1;
+				wordMap.put(words[i].toLowerCase(), value);
+			}else {
+				int value=1;
+				wordMap.put(words[i].toLowerCase(), value);
+			}
 		}
+		//寻找出现次数最多的word
+		String resultKey="";
+		int resultValue=0;
+		for(Map.Entry<String,Integer> entry: wordMap.entrySet()){
+			if(entry.getValue()>resultValue){
+				resultKey=entry.getKey();
+				resultValue=entry.getValue();
+			}
+			else if(!entry.getKey().equals("")&&!resultKey.equals("")&&entry.getValue()==resultValue&&entry.getKey().charAt(0)<resultKey.charAt(0)){
+				resultKey=entry.getKey();
+			}
+			//System.out.print(entry.getKey()+"-"+entry.getValue()+"\n");
+		}
+		System.out.println(resultKey+" "+resultValue);
 	}
 }
