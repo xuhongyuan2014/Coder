5a07a404adc11c611d17c9c9b0c5384b82b904cf
xuhongyuan <xuhongyuan666@163.com>
2015-09-19 23:39:37 +0800

    单例模式（饿汉、懒汉模式）

--- src/test55/Singleton.java
+++ src/test55/Singleton.java
@@ -1,14 +1,32 @@
 package test55;
-
+//饿汉模式---类加载时创建
 public class Singleton {
 	private Singleton() {
 	}
-	private static Singleton instence=new Singleton();
+	private static Singleton instance=new Singleton();
 	public static Singleton getSingleton(){
-		return  instence;
+		return  instance;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Singleton.getSingleton());
 }
 }
+/*
+ * 懒汉模式---需要获取时才创建
+ * public class Singleton {
+ *
+	private Singleton() {
+	}
+	private static Singleton instance=null;
+	public static Singleton getSingleton(){
+		if(instance==null){
+			instance=new Singleton();
+		}
+		return  instance;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Singleton.getSingleton());
+}
+}*/
