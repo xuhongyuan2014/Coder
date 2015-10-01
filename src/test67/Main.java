3f0f4362a67f32e383aa3caad3aa8f2c488f3420
xuhongyuan <xuhongyuan666@163.com>
2015-10-01 21:52:43 +0800

    全排列---------------递归

---
+++ src/test67/Main.java
@@ -0,0 +1,30 @@
+package test67;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*È«ÅÅÁÐ---------------µÝ¹é*/
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] data={"a","b","c","d"};
+		Sort(Arrays.asList(data),new ArrayList(),4);
+	}
+	public static void Sort(List data,List target,int num){
+		if(target.size()==num){
+			for(Object obj :target){
+				System.out.print(obj);
+			}
+			System.out.println();
+		}
+		for(int i=0;i<data.size();i++){
+			List newData=new ArrayList(data);
+			List newTarget=new ArrayList(target);
+			newTarget.add(data.get(i));
+			newData.remove(i);
+			Sort(newData,newTarget,num);
+		}
+	}
+}
