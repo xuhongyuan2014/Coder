d11f846cbd2c222880087828d1ff6245c4b195f3
xuhongyuan <xuhongyuan666@163.com>
2015-10-01 22:20:02 +0800

    Permutation Sequence

---
+++ src/test68/Main.java
@@ -0,0 +1,46 @@
+package test68;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s=new Solution();
+		System.out.println(s.getPermutation(3, 5));
+	}
+
+}
+class Solution {
+    public static int flag=0;//计数
+    public static String result;//存放结果
+    public String getPermutation(int n, int k) {
+        List data=new ArrayList();//转换成List
+        for(int i=1;i<=n;i++){
+        	data.add(i);
+        }
+        Sort(data,new ArrayList(),n,k);
+        return result;
+    }
+    public void Sort(List data,List target,int num,int k){//num位全排列，取第K次的结果
+        if(target.size()==num){
+            flag++;
+            if(flag==k){
+                StringBuilder s=new StringBuilder();
+                for(Object obj:target){
+                    s.append(obj);
+                }
+                result=s.toString();
+            }
+        }
+        for(int i=0;i<data.size();i++){//递归取
+            List newData=new ArrayList(data);
+            List newTarget=new ArrayList(target);
+            newTarget.add(newData.get(i));
+            newData.remove(i);
+            Sort(newData,newTarget,num,k);
+        }
+    }
+}
---
+++ src/test68/README.md
@@ -0,0 +1,16 @@
+Permutation Sequence My Submissions Question Solution 
+Total Accepted: 39300 Total Submissions: 169825 Difficulty: Medium
+The set [1,2,3,бн,n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order,
+We get the following sequence (ie, for n = 3):
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+Note: Given n will be between 1 and 9 inclusive.
