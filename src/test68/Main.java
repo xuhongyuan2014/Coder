62fdd16078c291492b1f3bc901d846b86686e531
xuhongyuan <xuhongyuan666@163.com>
2015-10-09 10:23:00 +0800

    Permutation Sequence

--- src/test68/Main.java
+++ src/test68/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution s=new Solution();
-		System.out.println(s.getPermutation(3, 5));
+		System.out.println(s.getPermutation(9, 13531));
 	}
 
 }
